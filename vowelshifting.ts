e3049e18718dc151ebe79fa69ab8bf79faac11e1
kasi <danielkaser@mailbox.org>
2023-07-18 15:10:17 +0200

    add undefined check to lastVowel in vowelshifting.ts

--- vowelshifting.ts
+++ vowelshifting.ts
@@ -1,4 +1,5 @@
 export function vowelShift(text: string | null, n: number): string | null {
+  // export function vowelShift(text: string | null, n: number): string {
   // loop through every letter of the input text
   // check if its a vowel or not
   // if yes, push it to an array
@@ -6,13 +7,13 @@ export function vowelShift(text: string | null, n: number): string | null {
   // loop through every letter of the input text again
   // replace every vowel with the elements from the array
 
-  console.log(text);
+  // console.log(text);
 
   const vowels = ["a", "e", "i", "o", "u"];
   //   let vowelsInInputText: string[] | null = []
   let vowelsInInputText: string[] = [];
 
-  //   console.log(vowelsInInputText)
+  ////   console.log(vowelsInInputText)
 
   if (text === null) return text;
 
@@ -24,34 +25,33 @@ export function vowelShift(text: string | null, n: number): string | null {
       vowelsInInputText.push(character);
     }
   }
-  //   if (vowelsInInputText.length === 0 || n === 0) return text
+  // if (vowelsInInputText.length === 0 || n === 0) return text
   if (!vowelsInInputText || n === 0) return text;
 
-  let lastVowel = "";
+  // let lastVowel = "";
+  let lastVowel: string | undefined;
 
   for (let i = 0; i < n; i++) {
-    console.log(vowelsInInputText);
-    lastVowel = vowelsInInputText.pop() as string;
-    console.log(lastVowel);
-    vowelsInInputText.unshift(lastVowel);
-    console.log(vowelsInInputText);
+    // console.log(vowelsInInputText);
+    lastVowel = vowelsInInputText.pop();
+    // console.log(lastVowel);
+    if (lastVowel !== undefined) {
+      vowelsInInputText.unshift(lastVowel as string);
+    }
+    // console.log(vowelsInInputText);
   }
-
-  //   for (let character of characters) {
-  //     if (vowels.includes(character)) {
-  //     }
-  //   }
 
   let outputText: string[] = [];
   //   const filteredArray: string[] = characters.filter((item): item is string => item !== undefined);
 
   if (vowelsInInputText) {
+    // if (!vowelsInInputText.includes("undefined")) {
     //     console.log(vowelsInInputText)
     console.log("-----------");
     outputText = characters.map((character) => {
       //       outputText = filteredArray.map((character) => {
-      //       if (vowels.includes(character)) {
-      if (vowels.includes(character) && vowelsInInputText !== null) {
+      if (vowels.includes(character)) {
+        // if (vowels.includes(character) && vowelsInInputText !== null) {
         //       if (vowels.includes(character) && vowelsInInputText !== undefined && vowelsInInputText !== null) {
         //         console.log(vowelsInInputText)
         return vowelsInInputText.shift();
