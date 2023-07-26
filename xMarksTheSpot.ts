228e3ced313e978c68baf4c590a53c4e775437a3
kasi <danielkaser@mailbox.org>
2023-07-26 11:12:16 +0200

    working attempt of xmarksthespot.ts

--- xMarksTheSpot.ts
+++ xMarksTheSpot.ts
@@ -8,32 +8,37 @@ export function x(n: number): number[][] {
 
   // create new line
   for (let i = 0; i < n; i++) {
-    console.log("lineNumber", i);
+    console.log("++lineNumber (i):", i);
     const newLine: number[] = [];
 
     // create new elements
     for (let j = 0; j < n; j++) {
       console.log("elementNumber (j):", j);
       console.log("newLine (before):", newLine);
-      console.log("------------------");
 
       // const newElement = 0;
 
       // only do this until half of the row
-      if (j <= n / 2) {
-        // if element index equals row index
-        if (j === i || j === n - i) {
-          newLine.push(1);
-        } else {
-          newLine.push(0);
-        }
+      //       if (j <= n / 2) {
+      // if element index equals row index
+      console.log("j===n-i-1:", j === n - i - 1);
+      if (j === i || j === n - i - 1) {
+        newLine.push(1);
+      } else {
+        newLine.push(0);
       }
+      //       } else {
+      //         newLine.push(0);
+      //       }
       console.log("newLine (after):", newLine);
-      output.push(newLine);
-      console.log("output:", output);
+      console.log("------------------");
+
       // if element has index i or array.length-i, element = 1, else 0
     }
+    output.push(newLine);
+    console.log("output:", output);
   }
 
+  console.log("\n");
   return output;
 }
