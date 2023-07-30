2fbc389aee48bf6d2934d0ef44353aa24614e6dd
kasi <danielkaser@mailbox.org>
2023-07-30 16:27:50 +0200

    solve welcome.ts

---
+++ welcome.ts
@@ -0,0 +1,27 @@
+type Greetings = {
+  [key: string]: string;
+};
+
+const greetings: Greetings = {
+  english: "Welcome",
+  czech: "Vitejte",
+  danish: "Velkomst",
+  dutch: "Welkom",
+  estonian: "Tere tulemast",
+  finnish: "Tervetuloa",
+  flemish: "Welgekomen",
+  french: "Bienvenue",
+  german: "Willkommen",
+  irish: "Failte",
+  italian: "Benvenuto",
+  latvian: "Gaidits",
+  lithuanian: "Laukiamas",
+  polish: "Witamy",
+  spanish: "Bienvenido",
+  swedish: "Valkommen",
+  welsh: "Croeso",
+};
+
+export function greet(language: string): string {
+  return greetings[language] ? greetings[language] : "Welcome";
+}
