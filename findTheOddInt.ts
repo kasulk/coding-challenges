b4e5e9437b997fe3eda2f64599cfd8f61fd8946c
kasi <danielkaser@mailbox.org>
2023-08-01 12:24:43 +0200

    solve findtheoddint.ts

--- findTheOddInt.ts
+++ findTheOddInt.ts
@@ -1,33 +1,21 @@
 export const findOdd = (xs: number[]): number => {
-  // happy coding!
-  // save the first number in variable
-  // if the next number is not the same
-  // check the length of the output array
-  // if its odd, return output[0]
-  // if even, wipe the array
+  // create an object
+  // for every new number in the array, add a new property with the number as its name
+  // initial value of the property is 'true'
+  // if number is found again, switch the propertys value to its opposite
+  // at the end, find the property with the value 'true'
 
-  if (xs.length === 1) return xs[0];
+  let isOdd: { [key: number]: boolean } = {};
 
-  xs.sort();
-
-  //note: too slow...
-
-  let output: number[] = [xs[0]];
-  console.log(xs[xs.length - 1]);
-
-  for (let i = 0; i < xs.length - 1; i++) {
-    //     console.log('=======================')
-    //     console.log('output before:', output)
-    //     console.log(xs[i], xs[i+1])
-
-    if (xs[i] !== xs[i + 1] && output.length % 2 !== 0) {
-      return output[0];
-    } else if (output.length % 2 === 0) {
-      output = [];
+  for (let element of xs) {
+    if (isOdd.hasOwnProperty(element)) {
+      isOdd[element] = !isOdd[element];
+    } else {
+      isOdd[element] = true;
     }
-    output.push(xs[i]);
-    //     console.log('output after:', output)
   }
-
-  return xs[xs.length - 1];
+  for (let key in isOdd) {
+    if (isOdd[key] === true) return Number(key);
+  }
+  return 0;
 };
