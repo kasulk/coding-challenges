c0091e7bc03111d1d57fe574ab2e15c1d4edb168
kasi <danielkaser@mailbox.org>
2023-08-08 15:46:34 +0200

    replace sum for loop with arithmetic sum formula in ismyfriendcheating.ts

--- isMyFriendCheating.ts
+++ isMyFriendCheating.ts
@@ -1,18 +1,19 @@
 export function removeNb(n: number): number[][] {
-  let sum = 0;
+  //
+  let sum = (n * (n + 1)) / 2;
   let output: [number, number][] = [];
 
-  for (let i = 1; i <= n; i++) {
-    sum += i;
-  }
+  //   for (let i = 1; i <= n; i++) {
+  //     sum += i;
+  //   }
 
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= n; j++) {
+  for (let i = n; i >= 1; i--) {
+    for (let j = n; j >= 1; j--) {
       if (i * j === sum - i - j) {
-        console.log(i, j);
+        // console.log(i, j);
         return (output = [
-          [i, j],
           [j, i],
+          [i, j],
         ]);
       }
     }
