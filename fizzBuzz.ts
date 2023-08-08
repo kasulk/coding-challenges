4be6600ca559577d263ae5316306eec2e2e94ad2
kasi <danielkaser@mailbox.org>
2023-08-08 11:33:54 +0200

    solve fizzbuzz.ts

---
+++ fizzBuzz.ts
@@ -0,0 +1,15 @@
+//note: hackerrank.com/
+function fizzBuzz(n: number): void {
+  //
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
