585fba8c7b9f04bd1cc592b2e5669cec8e0661c9
kasi <danielkaser@mailbox.org>
2023-08-15 10:21:07 +0200

    solve decodeTheMorseCode.ts

---
+++ decodeTheMorseCode.ts
@@ -0,0 +1,80 @@
+// import { MORSE_CODE } from "./preloaded";
+export function decodeMorse(morseCode: string): string {
+  // remove whitespace from start and end
+  // turn input into array of morse words and loop through it
+  // for each morse word
+  // turn each morse word into array of morse characters, and loop through it
+  // for each morse character
+  // return the value of the property that matches the morse character
+  // turn the letter arrays back into strings of words
+  // turn the words array back into string, return it
+
+  return morseCode
+    .trim()
+    .split("   ")
+    .map((morseWord) =>
+      morseWord
+        .split(" ")
+        .map((morseChar) => MORSE_CODE[morseChar])
+        .join("")
+    )
+    .join(" ");
+}
+
+const MORSE_CODE = {
+  "-.-.--": "!",
+  ".-..-.": '"',
+  "...-..-": "$",
+  ".-...": "&",
+  ".----.": "'",
+  "-.--.": "(",
+  "-.--.-": ")",
+  ".-.-.": "+",
+  "--..--": ",",
+  "-....-": "-",
+  ".-.-.-": ".",
+  "-..-.": "/",
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "---...": ":",
+  "-.-.-.": ";",
+  "-...-": "=",
+  "..--..": "?",
+  ".--.-.": "@",
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "..--.-": "_",
+  "...---...": "SOS",
+};
