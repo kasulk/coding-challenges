c5177d2ca98af27c90d57eda80512080a1cd56a7
kasi <danielkaser@mailbox.org>
2023-08-17 11:03:46 +0200

    solve vowelshifting.ts

--- vowelshifting.ts
+++ vowelshifting.ts
@@ -1,66 +1,42 @@
 export function vowelShift(text: string | null, n: number): string | null {
-  // export function vowelShift(text: string | null, n: number): string {
-  // loop through every letter of the input text
-  // check if its a vowel or not
-  // if yes, push it to an array
-  // remove the n last vowels in the array and move them to the start
-  // loop through every letter of the input text again
-  // replace every vowel with the elements from the array
+  // loop through string
+  // save each vowel in an array
+  // if n > 0, pop vowel array and unshift back n times
+  // if n < 0, shift vowel array and push back Math.abs(n) times
+  // loop through input string array again
+  // replace every vowel with the first vowel in the vowel array
 
-  // console.log(text);
+  if (!text || !n) return text;
 
-  const vowels = ["a", "e", "i", "o", "u"];
-  //   let vowelsInInputText: string[] | null = []
-  let vowelsInInputText: string[] = [];
+  const vowels = new RegExp(/[aeiouAEIOU]/);
+  const vowelsOfText: string[] = [];
 
-  ////   console.log(vowelsInInputText)
-
-  if (text === null) return text;
-
-  const characters: string[] = text.split("");
-  //   const characters = text.split('')
-
-  for (let character of characters) {
-    if (vowels.includes(character)) {
-      vowelsInInputText.push(character);
+  // collect vowels
+  for (let character of text) {
+    if (vowels.test(character)) {
+      vowelsOfText.push(character);
     }
   }
-  // if (vowelsInInputText.length === 0 || n === 0) return text
-  if (!vowelsInInputText || n === 0) return text;
 
-  // let lastVowel = "";
-  let lastVowel: string | undefined;
+  if (!vowelsOfText.length) return text;
 
-  for (let i = 0; i < n; i++) {
-    // console.log(vowelsInInputText);
-    lastVowel = vowelsInInputText.pop();
-    // console.log(lastVowel);
-    if (lastVowel !== undefined) {
-      vowelsInInputText.unshift(lastVowel as string);
+  // shift vowels
+  if (n < 0) {
+    for (let i = 0; i < Math.abs(n); i++) {
+      vowelsOfText.push(vowelsOfText.shift() as string);
     }
-    // console.log(vowelsInInputText);
+  }
+  if (n > 0) {
+    for (let i = 0; i < Math.abs(n); i++) {
+      vowelsOfText.unshift(vowelsOfText.pop() as string);
+    }
   }
 
-  let outputText: string[] = [];
-  //   const filteredArray: string[] = characters.filter((item): item is string => item !== undefined);
-
-  if (vowelsInInputText) {
-    // if (!vowelsInInputText.includes("undefined")) {
-    //     console.log(vowelsInInputText)
-    console.log("-----------");
-    outputText = characters.map((character) => {
-      //       outputText = filteredArray.map((character) => {
-      if (vowels.includes(character)) {
-        // if (vowels.includes(character) && vowelsInInputText !== null) {
-        //       if (vowels.includes(character) && vowelsInInputText !== undefined && vowelsInInputText !== null) {
-        //         console.log(vowelsInInputText)
-        return vowelsInInputText.shift();
-      } else {
-        return character;
-      }
-    });
-    console.log(outputText.join(""));
-  }
-
-  return outputText.join("");
+  // replace vowels in text with shifted vowels
+  return text
+    .split("")
+    .map((character) =>
+      vowels.test(character) ? (vowelsOfText.shift() as string) : character
+    )
+    .join("");
 }
