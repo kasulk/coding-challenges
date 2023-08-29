eacda512c283764c03ad10e76a6ee61a443cd9c0
kasi <danielkaser@mailbox.org>
2023-08-29 20:19:59 +0200

    solve oppositesAttract.ts

---
+++ oppositesAttract.ts
@@ -0,0 +1,6 @@
+export function lovefunc(flower1: number, flower2: number): boolean {
+  // add both numbers
+  // if the sum is odd, return true, else false
+
+  return (flower1 + flower2) % 2 ? true : false;
+}
