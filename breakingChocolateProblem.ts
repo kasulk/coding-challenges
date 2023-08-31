88071e19d6178534ebb0f68b50752da8ac01be28
kasi <danielkaser@mailbox.org>
2023-08-31 11:19:14 +0200

    solve breakingchocolateproblem.ts

---
+++ breakingChocolateProblem.ts
@@ -0,0 +1,8 @@
+export function breakChocolate(n: number, m: number): number {
+  // if n and m are valid
+  /// multiply n by m
+  /// subract 1
+  // else return 0
+
+  return n && m ? n * m - 1 : 0;
+}
