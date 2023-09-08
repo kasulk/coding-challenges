0cde7bdc60c41e7dfbe81e8ab82109daa0686f7e
kasi <danielkaser@mailbox.org>
2023-09-08 12:57:34 +0200

    refactor integersrecreationone

--- integersRecreationOne.ts
+++ integersRecreationOne.ts
@@ -1,48 +1,40 @@
-export const listSquared = (m: number, n: number): number[][] => {
+export const listSquared = (start: number, end: number): number[][] => {
   // loop through all integers from m to n
-  // for every number, loop through all integers from 1 to number
-  /// if the number is divisible by the integer
-  /// square the integer and add it to a sum variable
+  // for every number (i), loop through all integers from 1
+  //// to number --> timeout, instead:
+  //! until square root of the number (i)
+  /// if the number is divisible by the divisor
+  /// square the divisor and add it to a sum variable
+  ///! if the divisor isn't the same as number/divisor,
+  ///! add also (number/divisor) squared to the sum
   // after the inner loop, if the square root of the sum is an integer
-  // add the number and the sum as elements to an array
+  // add the number (i) and the sum as elements to an array
   // add this array to the output array
   // reset the sum variable
   // after the outer loop, return the output array
 
-  //? inner loop only once
-  //? are some numbers skippable
-  // check each number j if it is a divisor of each number i (m to n)
-  // if yes, square j
-
-  console.log(m, n);
-
   const output: [number, number][] = [];
   let sum = 0;
 
-  for (let i = m; i <= n; i++) {
-    // console.log("------------");
-    // console.log(i);
+  for (let i = start; i <= end; i++) {
+    //// for (let divisor = 1; j <= i; j++) {     // --> timeout, instead:
+    //!
+    for (let divisor = 1; divisor <= Math.floor(Math.sqrt(i)); divisor++) {
+      if (i % divisor === 0) {
+        sum += divisor ** 2;
 
-    // for (let j = 1; j <= i; j++) {
-    for (let j = 1; j <= Math.floor(Math.sqrt(i)); j++) {
-      //   console.log("j:", j, i % j === 0);
-
-      if (i % j === 0) {
-        sum += j * j;
-        // new:
-        if (j !== i / j) {
-          sum += (i / j) * (i / j);
+        //! if divisor is a divisor, add also i/divisor, unless they're the same
+        if (divisor !== i / divisor) {
+          sum += (i / divisor) ** 2; //!
         }
-        // console.log("sum:", sum);
       }
     }
-
-    // console.log(Number.isInteger(Math.sqrt(sum)));
 
     if (Number.isInteger(Math.sqrt(sum))) {
       output.push([i, sum]);
     }
     sum = 0;
   }
+
   return output;
 };
