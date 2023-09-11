a1bd2c0010d7c31d3d8ea2e967f4e02762822915
kasi <danielkaser@mailbox.org>
2023-09-11 17:45:46 +0200

    solve sumsofparts.ts again; but again with timeout...

--- sumsOfParts.ts
+++ sumsOfParts.ts
@@ -2,6 +2,9 @@ export function partsSums(ls: number[]): number[] {
   // loop through the list, for each number,
   // pop the last off the input list
   // add it to a sum variable and push the sum to the output array
+  //   console.log("----------------");
+
+  //   console.log(ls);
 
   let sum = 0;
 
@@ -9,9 +12,17 @@ export function partsSums(ls: number[]): number[] {
 
   if (ls.length === 0) return [0];
 
-  while (ls.length > 0) {
-    sum += ls.pop() ?? 0;
-    outputSums.unshift(sum);
+  //   while (ls.length > 0) {
+  // console.log("ls.length:", ls.length);
+
+  // sum += ls.pop() ?? 0;
+  // outputSums.unshift(sum);
+  // console.log("outputSums:", outputSums);
+
+  //   }
+
+  for (let i = ls.length - 1; i >= 0; i--) {
+    outputSums.unshift((sum += ls[i]));
   }
 
   return outputSums;
