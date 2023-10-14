c725a3e6366811c7e28c9530ec0a3ab596818f44
kasi <danielkaser@mailbox.org>
2023-10-14 20:42:02 +0200

    solve calculateBMI.ts

---
+++ codewars/calculateBMI.ts
@@ -0,0 +1,8 @@
+export function bmi(weight: number, height: number): string {
+  const bmi = weight / height ** 2;
+
+  if (bmi <= 18.5) return "Underweight";
+  if (bmi <= 25) return "Normal";
+  if (bmi <= 30) return "Overweight";
+  return "Obese";
+}
