4021a0aa9065c5947f54d05553229d73bffbcb39
kasi <danielkaser@mailbox.org>
2023-10-17 17:43:05 +0200

    refactor wearetherobots.ts

--- codewars/weAreTheRobots.ts
+++ codewars/weAreTheRobots.ts
@@ -1,45 +1,30 @@
-export function countRobots(a: string[]): string[] {
-  // 2do: count how many elements in the input array contain
-  // a functioning robot and the word automatik or
-  // a functioning robot and the word mechanic
-  //
-  // loop through input array
-  // for each element
-  // count the valid robots in the string
-  //
-  console.log("input:", a);
-  let bla = {
+export function countRobots(strs: string[]): string[] {
+  // create a counter object with automatik and mechanik as properties and 0 as values
+  // create a regex search pattern for the robots
+  // loop through the input string array, for each string
+  // if it contains a valid robot and either the text 'automatik' or 'mechanik'
+  // increase the corresponding counter in the counter object
+  // return an array with values of the counter object
+
+  const foundRobotsCounts = {
     automatik: 0,
     mechanik: 0,
   };
-  let foundRobotsCounts;
-  //   const robotRegEx = /[a-z]\W\W0\W\W0\W\W[a-z]/gi;
+
   const robotRegEx = /[a-z][^\w\s]{2}0[^\w\s]{2}0[^\w\s]{2}[a-z]/gi;
 
-  if (!a.length) foundRobotsCounts = [0, 0];
-  else {
-    a.forEach((element) => {
-      //   element.match(robotRegEx) && /automatik|mechanik/i.test(element)
-      //     ? element.match(robotRegEx)?.length
-      //     : 0
-      if (element.match(robotRegEx) && /automatik/i.test(element))
-        bla.automatik = element.match(robotRegEx)?.length!;
-      if (element.match(robotRegEx) && /mechanik/i.test(element))
-        bla.mechanik = element.match(robotRegEx)?.length!;
-    });
-  }
+  strs.forEach((str) => {
+    const foundRobots = str.match(robotRegEx);
 
-  //   console.log("foundRobotsCounts:", foundRobotsCounts);
-  console.log("bla:", bla);
+    if (foundRobots && /automatik/i.test(str))
+      foundRobotsCounts.automatik += foundRobots.length!;
 
-  const output = [
-    // `${foundRobotsCounts[0]} robots functioning automatik`,
-    // `${foundRobotsCounts[1]} robots dancing mechanik`,
-    `${bla.automatik} robots functioning automatik`,
-    `${bla.mechanik} robots dancing mechanik`,
+    if (foundRobots && /mechanik/i.test(str))
+      foundRobotsCounts.mechanik += foundRobots.length!;
+  });
+
+  return [
+    `${foundRobotsCounts.automatik} robots functioning automatik`,
+    `${foundRobotsCounts.mechanik} robots dancing mechanik`,
   ];
-  console.log("output:", output);
-  console.log("------------------------");
-
-  return output;
 }
