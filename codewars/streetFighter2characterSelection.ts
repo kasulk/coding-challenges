62128fc1cf6a296e62eed3f0a8af25ed0326d396
kasi <danielkaser@mailbox.org>
2023-11-02 10:44:11 +0100

    refactor streetfighter2characterselection.ts

--- codewars/streetFighter2characterSelection.ts
+++ codewars/streetFighter2characterSelection.ts
@@ -8,17 +8,14 @@ export function streetFighterSelection(
   // if the cursor moves up, subtract 1 from the first element of the position array
   // if the cursor moves down, add 1 to the first element of the position array
   // the first element can only be 0 or 1
-  // same in horizontal direction, but after 6 comes 1 again
+  // same in direction, but after 5 comes 0 again
+  // and before 0 is 5
 
   let [y, x] = position;
 
   return moves.map((move) => {
-    if (y === 1) {
-      if (move === "up") y -= 1;
-    }
-    if (y === 0) {
-      if (move === "down") y += 1;
-    }
+    if (move === "up" && y === 1) y -= 1;
+    if (move === "down" && y === 0) y += 1;
     if (move === "right") x = (x + 1) % 6;
     if (move === "left") {
       if (x === 0) x = 6;
