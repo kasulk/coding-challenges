6646133c6f34556e8355d77bef14651be42cbe88
kasi <danielkaser@mailbox.org>
2023-11-11 14:03:12 +0100

    solve rockpaperscissors.ts

---
+++ codewars/rockPaperScissors.ts
@@ -0,0 +1,12 @@
+export function rps(p1: string, p2: string): string {
+  if (p1 === p2) return "Draw!";
+
+  if (
+    (p1 === "rock" && p2 === "scissors") ||
+    (p1 === "paper" && p2 === "rock") ||
+    (p1 === "scissors" && p2 === "paper")
+  )
+    return "Player 1 won!";
+
+  return "Player 2 won!";
+}
