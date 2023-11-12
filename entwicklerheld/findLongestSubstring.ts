59f6031728a6370fcc9438b66d6b5c1cffadfa56
kasi <danielkaser@mailbox.org>
2023-11-12 19:51:36 +0100

    solve findlongestsubstring.ts

--- entwicklerheld/findLongestSubstring.ts
+++ entwicklerheld/findLongestSubstring.ts
@@ -1,4 +1,4 @@
-export function findLongestSubstring(string1, string2): string[] {
+export function findLongestSubstring(string1, string2): string {
   // for each character in the input string1
   // check if it is included in input string2
   // if yes
@@ -15,44 +15,37 @@ export function findLongestSubstring(string1, string2): string[] {
   // repeat with the next character (after the length of the current) in string1
   // return the output
 
+  string1.toUpperCase();
+  string2.toUpperCase();
+
   let output: string[] = [];
 
   for (let i = 0; i < string1.length; i++) {
     // if (string1.slice(i, string1.length) < output[0].length) break;
-
     const currChar = string1[i];
 
     if (!string2.includes(currChar)) continue;
 
-    // if (string2.includes(currChar)) {
     let currSubstring = currChar;
 
     while (string2.includes(currSubstring)) {
       i++;
-      if (!string1[i]) break;
+      const nextChar = string1[i];
 
-      currSubstring += string1[i];
+      if (!nextChar) break;
+
+      currSubstring += nextChar;
     }
     i--;
-    currSubstring = currSubstring.slice(0, -1); // remove last checked letter
-    //   console.log("curr", currSubstring);
-    // }
+    if (i !== string1.length - 1) currSubstring = currSubstring.slice(0, -1); // remove last checked letter
 
     if (output[0] && output[0].length < currSubstring.length) {
       output = [currSubstring];
+      //
     } else if (!output[0] || output[0].length === currSubstring.length) {
       output.push(currSubstring);
     }
-    // console.log("output", output);
-
-    i += currSubstring.length - 1;
   }
 
-  return output;
+  return output.join(" ");
 }
-
-findLongestSubstring(
-  "ADREICHINESENMITDEMKONTRABASSSASSENAUFDERSTRASSE",
-  // 'ADREIAAPOPE',
-  "PALIMPALIMDREIPOPEI"
-);
