09520be6f54de147644457391ac15ecc29d1b7c8
kasi <danielkaser@mailbox.org>
2023-11-13 19:14:37 +0100

    solve findlongestsubstring.ts

--- entwicklerheld/findLongestSubstring.ts
+++ entwicklerheld/findLongestSubstring.ts
@@ -4,15 +4,18 @@ export function findLongestSubstring(string1, string2): string {
   // if yes
   // add the next letter of string1 and check string2 again
   // if not
-  // check the length of the first element in output array
-  // if it it shorter than the current substring
-  // overwrite the content of the output array with the current substring
-  // if its length is equal to the current substring or if there is no element
-  // add the current substring to the output array
-  //? (if at some point the rest of string1
-  //? is shorter than the already found longest substring,
-  //? end the loop prematurely)
-  // repeat with the next character (after the length of the current) in string1
+
+  // if the output array is empty or
+  // if the length of the current substring is greater than
+  // the length of an element in the output array
+  /// clear the output array and assign it the current substring
+  // else if
+  /// the length of the current substring is equal to
+  /// the length of an element in the output array
+  /// add the current substring to the output array
+
+  // repeat with the next character in string1
+  // (after the length of the current)
   // return the output
 
   string1.toUpperCase();
@@ -21,7 +24,6 @@ export function findLongestSubstring(string1, string2): string {
   let output: string[] = [];
 
   for (let i = 0; i < string1.length; i++) {
-    // if (string1.slice(i, string1.length) < output[0].length) break;
     const currChar = string1[i];
 
     if (!string2.includes(currChar)) continue;
@@ -31,21 +33,35 @@ export function findLongestSubstring(string1, string2): string {
     while (string2.includes(currSubstring)) {
       i++;
       const nextChar = string1[i];
-
       if (!nextChar) break;
 
       currSubstring += nextChar;
     }
     i--;
-    if (i !== string1.length - 1) currSubstring = currSubstring.slice(0, -1); // remove last checked letter
 
-    if (output[0] && output[0].length < currSubstring.length) {
+    if (!isVeryLastCharOfInputStr(string1, i))
+      currSubstring = removeLastCharFromStr(currSubstring); // remove last checked letter
+
+    if (isEmpty(output) || currSubstring.length > output[0].length)
       output = [currSubstring];
-      //
-    } else if (!output[0] || output[0].length === currSubstring.length) {
+    else if (currSubstring.length === output[0].length)
       output.push(currSubstring);
-    }
   }
 
   return output.join(" ");
+}
+
+//
+function removeLastCharFromStr(str: string): string {
+  return str.slice(0, -1);
+}
+
+//
+function isEmpty(arr: string[]): boolean {
+  return !Boolean(arr.length);
+}
+
+//
+function isVeryLastCharOfInputStr(str: string, index: number): boolean {
+  return index === str.length - 1;
 }
