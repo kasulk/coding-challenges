54c44fcb00d725e54fbd86e78d223773e56d3a04
kasi <danielkaser@mailbox.org>
2023-11-13 19:30:43 +0100

    refactor findlongestsubstring.ts

--- entwicklerheld/findLongestSubstring.ts
+++ entwicklerheld/findLongestSubstring.ts
@@ -1,8 +1,8 @@
 export function findLongestSubstring(string1, string2): string {
-  // for each character in the input string1
-  // check if it is included in input string2
+  // for each character in the shortStr
+  // check if it is included in longStr
   // if yes
-  // add the next letter of string1 and check string2 again
+  // add the next letter of shortStr and check longStr again
   // if not
 
   // if the output array is empty or
@@ -14,32 +14,33 @@ export function findLongestSubstring(string1, string2): string {
   /// the length of an element in the output array
   /// add the current substring to the output array
 
-  // repeat with the next character in string1
+  // repeat with the next character in shortStr
   // (after the length of the current)
   // return the output
 
-  string1.toUpperCase();
-  string2.toUpperCase();
+  const [shortStr, longStr] = findShorterString(string1, string2);
+  shortStr.toUpperCase();
+  longStr.toUpperCase();
 
   let output: string[] = [];
 
-  for (let i = 0; i < string1.length; i++) {
-    const currChar = string1[i];
+  for (let i = 0; i < shortStr.length; i++) {
+    const currChar = shortStr[i];
 
-    if (!string2.includes(currChar)) continue;
+    if (!longStr.includes(currChar)) continue;
 
     let currSubstring = currChar;
 
-    while (string2.includes(currSubstring)) {
+    while (longStr.includes(currSubstring)) {
       i++;
-      const nextChar = string1[i];
+      const nextChar = shortStr[i];
       if (!nextChar) break;
 
       currSubstring += nextChar;
     }
     i--;
 
-    if (!isVeryLastCharOfInputStr(string1, i))
+    if (!isVeryLastCharOfInputStr(shortStr, i))
       currSubstring = removeLastCharFromStr(currSubstring); // remove last checked letter
 
     if (isEmpty(output) || currSubstring.length > output[0].length)
@@ -49,6 +50,12 @@ export function findLongestSubstring(string1, string2): string {
   }
 
   return output.join(" ");
+}
+
+//
+function findShorterString(str1: string, str2: string): string[] {
+  if (str1.length < str2.length) return [str1, str2];
+  return [str2, str1];
 }
 
 //
@@ -65,3 +72,8 @@ function isEmpty(arr: string[]): boolean {
 function isVeryLastCharOfInputStr(str: string, index: number): boolean {
   return index === str.length - 1;
 }
+
+findLongestSubstring(
+  "CHECKITWHEREISTHELONGESTSUBSTRING24",
+  "SUBSTINGWHERECHECKITANTCHECK24ISCHECKWHERE"
+); // checkit
