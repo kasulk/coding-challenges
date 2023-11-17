055f98c927307656d14c926e15c33134fbdaf780
kasi <danielkaser@mailbox.org>
2023-11-17 13:06:23 +0100

    solve react display array of users to browser

--- 10-react-coding-challenges/02-display-array-of-users-to-browser/src/App.tsx
+++ 10-react-coding-challenges/02-display-array-of-users-to-browser/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+  Challenge: Display all users to the browser
+  Small Hint: Use array map with react key
+**/
 
-function App() {
-  const [count, setCount] = useState(0)
+const users = [
+  { name: "John Doe", id: 1 },
+  { name: "Jane Doe", id: 2 },
+  { name: "Billy Doe", id: 3 },
+];
 
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>User names</h3>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
-
-export default App
