f8db7c0db492bdd256228781835f2c2df3e457b6
kasi <danielkaser@mailbox.org>
2023-11-22 10:54:54 +0100

    refactor comparestringsbysumsofchars.ts

--- codewars/compareStringsBySumOfChars.ts
+++ codewars/compareStringsBySumOfChars.ts
@@ -1,17 +1,17 @@
 export function compare(str1: string | null, str2: string | null): boolean {
-  const sums = [0, 0];
+  const inputs = [str1, str2];
+  let charCodeSums = [0, 0];
 
-  if (str1 && !/[^a-z]/i.test(str1))
-    sums[0] = str1
-      ?.toUpperCase()
-      .split("")
-      .reduce((charCodeSum, letter) => charCodeSum + letter.charCodeAt(0), 0);
+  for (let i = 0; i < inputs.length; i++) {
+    const currStr = inputs[i];
 
-  if (str2 && !/[^a-z]/i.test(str2))
-    sums[1] = str2
-      ?.toUpperCase()
-      .split("")
-      .reduce((charCodeSum, letter) => charCodeSum + letter.charCodeAt(0), 0);
+    if (currStr && !/[^a-z]/i.test(currStr)) {
+      charCodeSums[i] = currStr
+        .toUpperCase()
+        .split("")
+        .reduce((charCodeSum, letter) => charCodeSum + letter.charCodeAt(0), 0);
+    }
+  }
 
-  return sums[0] === sums[1];
+  return charCodeSums[0] === charCodeSums[1];
 }
