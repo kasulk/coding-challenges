ce36e80cf75f60c0d8793aabea31a58ea050f8d0
kasi <danielkaser@mailbox.org>
2023-11-23 16:40:50 +0100

    finish time calculation and toString() method on buildaclock.ts class

--- entwicklerheld/buildAClock.ts
+++ entwicklerheld/buildAClock.ts
@@ -1,14 +1,13 @@
 export class Clock {
   private hours: number; //!
   private minutes: number; //!
-  // private timeHoursString: string; //!
-  // private timeMinutesString: string; //!
-  private hoursTemp: number; //!
-  private minsTemp: number; //!
-
-  private totalMins: number;
-  private timeHrs: number;
-  private timeMins: number;
+  // private hoursTemp: number; //!
+  // private minsTemp: number; //!
+  // private totalMins: number;
+  // private timeHrs: number;
+  // private timeMins: number;
+  private clockMinutes: number;
+  private clockHours: number;
 
   constructor(hours = 0, minutes = 0) {
     this.hours = hours;
@@ -16,24 +15,52 @@ export class Clock {
   }
 
   private _calcTime(): void {
-    this.minsTemp = this.minutes; //!neu
-    if (this.minutes < 0) this.minsTemp = this.minutes % 60; //!neu
+    // this.minsTemp = this.minutes; //!neu
+    // if (this.minutes < 0) this.minsTemp = this.minutes % 60; //!neu
 
-    // this.hoursTemp = this.hours % 24;
-    this.hoursTemp = (this.hours % 24) + Math.trunc(this.minutes / 60); //!neu
-    if (this.hoursTemp < 0) this.hoursTemp += 24;
+    // // this.hoursTemp = this.hours % 24;
+    // this.hoursTemp = (this.hours % 24) + Math.trunc(this.minutes / 60); //!neu
+    // if (this.hoursTemp < 0) this.hoursTemp += 24; //? bug? s. runjs
 
-    // this.totalMins = this.hoursTemp * 60 + this.minutes;
-    this.totalMins = this.hoursTemp * 60 + this.minsTemp; //!neu
-    this.timeMins = this.totalMins % 60;
-    this.timeHrs = Math.trunc(this.totalMins / 60) % 24;
+    // // this.totalMins = this.hoursTemp * 60 + this.minutes;
+    // this.totalMins = this.hoursTemp * 60 + this.minsTemp; //!neu
+    // this.timeMins = this.totalMins % 60;
+    // this.timeHrs = Math.trunc(this.totalMins / 60) % 24;
+    //// return this.time;
 
-    // return this.time;
+    console.log("input:", this.hours, this.minutes);
+    // convert the hours into minutes (hours*60)
+    // add/subtract the input minutes
+    let totalTimeInMins = this.hours * 60 + this.minutes;
+    // console.log(totalTimeInMins);
+    // totalTimeInMins += mins;
+    // console.log(totalTimeInMins);
+    // divide the result by 60 to get the actual hours (before comma)
+    let totalHours = Math.trunc(totalTimeInMins / 60);
+    // console.log(totalHours);
+    // apply modulo 60 to get the actual minutes
+    this.clockMinutes = totalTimeInMins % 60;
+    // console.log(this.clockMinutes);
+    // if the mins are smaller 0, subtract them from 60 (and subtract 1 from the hours)
+    if (this.clockMinutes < 0) {
+      this.clockMinutes += 60;
+      totalHours--;
+    }
+    // console.log(this.clockMinutes);
+    //
+    // apply modulo 24 to the hours
+    this.clockHours = totalHours % 24;
+    // console.log(this.clockHours);
+    // if the resulting hours smaller 0, subtract them from 24
+    if (this.clockHours < 0) this.clockHours += 24;
+    // console.log(this.clockHours);
+
+    // console.log(this.clockHours + ":" + this.clockMinutes);
   }
 
   toString(): string {
     this._calcTime();
-    return `${this.timeHrs.toString().padStart(2, "0")}:${this.timeMins
+    return `${this.clockHours.toString().padStart(2, "0")}:${this.clockMinutes
       .toString()
       .padStart(2, "0")}`;
   }
@@ -81,3 +108,46 @@ export class Clock {
 // }
 
 // convertTime(hrs, mins)
+
+//// apply modulo 60 to the minutes, to get the remainding minutes
+//// divide the minutes by 60 to get the hours that have to be added/subracted to the input hours
+
+// convert the hours into minutes (hours*60)
+// add/subtract the input minutes
+// divide the result by 60 to get the actual hours (before comma)
+// apply modulo 60 to get the actual minutes
+//
+// apply modulo 24 to the hours
+// if the resulting hours smaller 0, subtract the from 24
+
+// function convertTime2(hrs, mins) {
+//   console.log("input:", hrs, mins);
+//   // convert the hours into minutes (hours*60)
+//   // add/subtract the input minutes
+//   let totalTimeInMins = hrs * 60 + mins;
+//   console.log(totalTimeInMins);
+//   // totalTimeInMins += mins;
+//   // console.log(totalTimeInMins);
+//   // divide the result by 60 to get the actual hours (before comma)
+//   let totalHours = Math.trunc(totalTimeInMins / 60);
+//   console.log(totalHours);
+//   // apply modulo 60 to get the actual minutes
+//   let clockMins = totalTimeInMins % 60;
+//   console.log(clockMins);
+//   // if the mins are smaller 0, subtract them from 60 (and subtract 1 from the hours)
+//   if (clockMins < 0) {
+//     clockMins += 60;
+//     totalHours--;
+//   }
+//   console.log(clockMins);
+//   //
+//   // apply modulo 24 to the hours
+//   let clockHours = totalHours % 24;
+//   console.log(clockHours);
+//   // if the resulting hours smaller 0, subtract the from 24
+//   if (clockHours < 0) clockHours += 24;
+//   console.log(clockHours);
+
+//   console.log(clockHours + ":" + clockMins);
+// }
+// convertTime2(hrs,mins)
