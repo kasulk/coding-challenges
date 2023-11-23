5d859d78cd85e1617eb388202f7324b8618e7c0c
kasi <danielkaser@mailbox.org>
2023-11-23 12:31:49 +0100

    refactor howmanyaresmallerthanme.ts

--- codewars/howManyAreSmallerThanMe.ts
+++ codewars/howManyAreSmallerThanMe.ts
@@ -3,16 +3,14 @@ export function smaller(nums: number[]): number[] {
   // loop through the rest of the array
   /// if the element is smaller, increase a counter by 1
   /// add the value of the counter to the new array
-  /// reset the counter
   // after the loops, return the output array
 
-  return nums.map((num, i) => {
-    let numSmaller = 0;
-
-    nums.slice(i + 1).forEach((element) => {
-      if (element < num) numSmaller++;
-    });
-
-    return numSmaller;
-  });
+  return nums.map((num, i) =>
+    nums
+      .slice(i + 1)
+      .reduce(
+        (smallerCount, curr) => (curr < num ? smallerCount + 1 : smallerCount),
+        0
+      )
+  );
 }
