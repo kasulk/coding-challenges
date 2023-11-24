a0522d803e740444d6db9d7c35d5956c44761d4b
kasi <danielkaser@mailbox.org>
2023-11-24 10:54:19 +0100

    solve seeyounexthappyyear.ts

---
+++ codewars/seeYouNextHappyYear.ts
@@ -0,0 +1,16 @@
+export function nextHappyYear(year: number): number {
+  // loop through years starting at year+1
+  // convert the each year into an array of digits
+  // create another array of it with only unique digits (= new set)
+  // if the length of both arrays is the same
+  // quit the loop and return the current year
+
+  while (year) {
+    year++;
+    const digits = [...year.toString()];
+    const uniqueDigits = [...new Set(digits)];
+    if (uniqueDigits.length === digits.length) break;
+  }
+
+  return year;
+}
