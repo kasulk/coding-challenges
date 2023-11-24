ae2a69920c671734eca15636f16344172d01b285
kasi <danielkaser@mailbox.org>
2023-11-24 13:43:15 +0100

    solve react 07 dynamically add child component

--- 10-react-coding-challenges/07-dynamically-add-child-components/src/App.tsx
+++ 10-react-coding-challenges/07-dynamically-add-child-components/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface IParentProps {
+  children: React.ReactNode;
+}
+/**
+  Challenge: 
+  Show entire Child component content inside Parent component. Only add code on Parent Component below
+**/
+function Child() {
+  return <div>This is children content</div>;
 }
 
-export default App
+// Add code only here
+function Parent({ children }: IParentProps) {
+  return (
+    <div>
+      <h3>Parent Component</h3>
+      <>{children}</>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Parent>
+      <Child />
+    </Parent>
+  );
+}
