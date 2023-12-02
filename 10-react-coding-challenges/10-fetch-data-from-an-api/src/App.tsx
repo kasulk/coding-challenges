d46c9ddff12078ce703a49f50794fcd90fc4b5e4
kasi <danielkaser@mailbox.org>
2023-12-02 18:35:18 +0100

    solve react 10 fetch from api

--- 10-react-coding-challenges/10-fetch-data-from-an-api/src/App.tsx
+++ 10-react-coding-challenges/10-fetch-data-from-an-api/src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const url = "https://jsonplaceholder.typicode.com/users/1";
+/** 
+  // Sample Response
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org"
+  }
+**/
 
+type UserData = { [key: string]: string };
+
+/** 
+  Challenge:
+   Given the url above, make this app fetch the data and display them in the browser (Small Hint: use `React.useEffect and fetch api`)
+**/
+export default function App() {
+  const [userData, setUserData] = useState<UserData>({});
+
+  useEffect(() => {
+    async function fetchUserData() {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchUserData();
+  }, []);
+
+  // No need to touch code below
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="App">
+      <h2>User Data</h2>
+      <p>
+        <strong>Name: </strong>{" "}
+        {userData.name || "(Need to populate name here)"}
       </p>
-    </>
-  )
+      <p>
+        <strong>Website: </strong>
+        {userData.website || "(Need to populate website here)"}
+      </p>
+      <p>
+        <strong>Email: </strong>
+        {userData.email || "(Need to populate email here)"}
+      </p>
+      <p>
+        <strong>Phone: </strong>
+        {userData.phone || "(Need to populate phone here)"}
+      </p>
+    </div>
+  );
 }
-
-export default App
