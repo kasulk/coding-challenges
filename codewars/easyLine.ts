47a47988cea418cb1fef15129269007a47dbc2d1
kasi <danielkaser@mailbox.org>
2023-12-26 13:12:34 +0100

    chatti solved easyLine.ts

--- codewars/easyLine.ts
+++ codewars/easyLine.ts
@@ -1,21 +1,12 @@
 export function easyLine(n: number): number {
-  console.log(n);
+  // chatti
 
-  let sum = 0;
+  let result = 1;
 
-  for (let i = 0; i <= n; i++) {
-    sum += getBinomialCoefficient(n, i) ** 2;
+  for (let i = 1; i <= n; i++) {
+    result *= n + i;
+    result /= i;
   }
 
-  return Math.round(Math.log(sum));
-}
-
-function factorial(n: number): number {
-  return Array(n)
-    .fill(0)
-    .reduce((prod, _, i) => prod * (i + 1), 1);
-}
-
-function getBinomialCoefficient(line: number, index: number): number {
-  return factorial(line) / (factorial(index) * factorial(line - index));
+  return Math.round(Math.log(result));
 }
