038caaeff4540b93eefbcf4174404181b5f2945e
kasi <danielkaser@mailbox.org>
2023-12-29 11:47:45 +0100

    solve unflattenalist-easy.ts

---
+++ codewars/unflattenAList-easy.ts
@@ -0,0 +1,26 @@
+export function unflatten(flatArray: number[]): number[] | number[][] {
+  const output: any[] = [];
+
+  for (let i = 0; i < flatArray.length; i++) {
+    const curr = flatArray[i];
+
+    if (curr < 3) {
+      output.push(curr);
+      continue;
+    }
+
+    if (curr > 2) {
+      const subArray: number[] = [];
+
+      for (let j = 0; j < curr; j++) {
+        const subCurr = flatArray[j + i];
+        if (subCurr) subArray.push(subCurr);
+      }
+
+      output.push(subArray);
+      i += curr - 1;
+    }
+  }
+
+  return output;
+}
