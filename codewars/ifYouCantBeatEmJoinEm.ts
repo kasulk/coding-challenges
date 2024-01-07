8ac1de1715160aec40f22d07c006cb272ea7e378
kasi <danielkaser@mailbox.org>
2024-01-07 10:53:17 +0100

    refactor ifyoucantbeatemjoinem.ts

--- codewars/ifYouCantBeatEmJoinEm.ts
+++ codewars/ifYouCantBeatEmJoinEm.ts
@@ -1,5 +1,5 @@
 export function cantBeatSoJoin(nums: number[][]): number[] {
-  return nums
+  return [...nums]
     .sort((a, b) => {
       const sumA = a.reduce((sum, curr) => sum + curr, 0);
       const sumB = b.reduce((sum, curr) => sum + curr, 0);
