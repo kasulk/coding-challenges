0127b840bbbdcac06c1fa5edb6af39774009e4ef
kasi <danielkaser@mailbox.org>
2024-01-28 10:16:42 +0100

    solve leonardodicaprioandoscars.ts

---
+++ codewars/leonardoDicaprioAndOscars.ts
@@ -0,0 +1,6 @@
+export const leo = (oscar: number): string => {
+  if (oscar === 88) return "Leo finally won the oscar! Leo is happy";
+  if (oscar === 86) return "Not even for Wolf of wallstreet?!";
+  if (oscar < 88) return "When will you give Leo an Oscar?";
+  return "Leo got one already!";
+};
