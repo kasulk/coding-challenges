a5b1d1bd8e6fe2f99f4f62e96524784308f6cc71
kasi <danielkaser@mailbox.org>
2024-02-01 13:06:24 +0100

    solve safenuserinputpartihtmlspecialchars.ts

---
+++ codewars/safenUserInputPartIhtmlSpecialChars.ts
@@ -0,0 +1,7 @@
+export function htmlspecialchars(formData: string): string {
+  return formData
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;");
+}
