16bece292706021c1c1f82f2918f9813cfd382ea
kasi <danielkaser@mailbox.org>
2024-03-05 20:08:45 +0100

    refactor rainfall.ts

--- codewars/rainfall.ts
+++ codewars/rainfall.ts
@@ -1,36 +1,36 @@
-export const mean = (town: string, str: string): number => {
+export function mean(town: string, str: string): number {
   const values = getValues(town, str);
+  if (!values) return -1;
 
-  if (!Array.isArray(values)) return -1;
-  return values.reduce((acc, curr) => acc + curr) / 12;
-};
+  return values.reduce((sum, value) => sum + value) / 12;
+}
 
-export const variance = (town: string, str: string): number => {
+export function variance(town: string, str: string): number {
   const values = getValues(town, str);
-
-  if (values === -1) return -1;
+  if (!values) return -1;
 
   const average = mean(town, str);
-  const teset = values.map((value) => Math.abs(average - value) ** 2);
-  //   const quads = teset.map((e) => e * e);
 
-  //   return quads.reduce((acc, curr) => acc + curr) / 12;
-  return teset.reduce((acc, curr) => acc + curr) / 12;
-};
+  return (
+    values
+      .map((value) => Math.abs(average - value) ** 2)
+      .reduce((sum, squaredDeviation) => sum + squaredDeviation) / 12
+  );
+}
 
-export function getValues(town: string, str: string): number[] | -1 {
+export function getValues(town: string, str: string): number[] | null {
   const [city, data] = str
     .split("\n")
     .filter((line) => line.includes(town))
     .join()
     .split(":");
 
-  if (city !== town) return -1;
+  if (city !== town) return null;
 
-  const values = data
+  const nakedValues = data
     .replace(/[a-z\s]/gi, "")
     .split(",")
     .map(Number);
 
-  return values;
+  return nakedValues;
 }
