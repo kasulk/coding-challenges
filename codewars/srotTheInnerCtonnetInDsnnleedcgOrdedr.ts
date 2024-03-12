188ba84d75d970be26944fcdd5ecf4eb36aa0062
kasi <danielkaser@mailbox.org>
2024-03-12 18:44:20 +0100

    refactor srotTheInnerCtonnetInDsnnleedcgOrdedr.ts

--- codewars/srotTheInnerCtonnetInDsnnleedcgOrdedr.ts
+++ codewars/srotTheInnerCtonnetInDsnnleedcgOrdedr.ts
@@ -3,22 +3,21 @@ export function sortTheInnerContent(words: string): string {
     .split(" ")
     .map((word) => {
       if (word.length <= 2) return word;
-      const first = word[0];
-      const last = word.slice(-1);
-      const middle = word.slice(1, -1);
 
-      return (
-        first +
-        middle
-          .split("")
-          .sort((a, b) => {
-            if (a > b) return -1;
-            if (a < b) return 1;
-            return 0;
-          })
-          .join("") +
-        last
-      );
+      const first = word[0];
+      const middle = word.slice(1, -1);
+      const last = word.slice(-1);
+
+      const sortedMiddle = middle
+        .split("")
+        .sort((a, b) => {
+          if (a < b) return 1;
+          if (a > b) return -1;
+          return 0;
+        })
+        .join("");
+
+      return first + sortedMiddle + last;
     })
     .join(" ");
 }
