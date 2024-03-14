4f0d9f78283fdf05645ed940b98f5fd1d13c7072
kasi <danielkaser@mailbox.org>
2024-03-14 18:40:48 +0100

    refactor prizedraw.ts

--- codewars/prizeDraw.ts
+++ codewars/prizeDraw.ts
@@ -12,15 +12,13 @@ export function rank(namesStr: string, weights: number[], n: number): string {
       (sum, currLetter) => sum + alphabet.indexOf(currLetter.toLowerCase()) + 1,
       0
     );
-
     const winNum = (lettersSum + name.length) * weights[i];
 
     return { name, winNum };
   });
 
   const sortedNamesWithPoints = namesWithPoints.sort((a, b) => {
-    if (b.winNum !== a.winNum) return b.winNum - a.winNum;
-    return a.name.localeCompare(b.name);
+    return b.winNum - a.winNum || a.name.localeCompare(b.name);
   });
 
   return sortedNamesWithPoints[n - 1].name;
