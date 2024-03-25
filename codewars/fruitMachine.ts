863c16b4a7b6647ecedb00632435c6fb952335c7
kasi <danielkaser@mailbox.org>
2024-03-25 11:56:33 +0100

    refactor fruitmachine.ts

--- codewars/fruitMachine.ts
+++ codewars/fruitMachine.ts
@@ -1,15 +1,19 @@
 export function fruit(reels: string[][], spins: number[]): number {
-  const itemsMap =
-    "_,Jack,Queen,King,Bar,Cherry,Seven,Shell,Bell,Star,Wild".split(",");
+  const items = "_,Jack,Queen,King,Bar,Cherry,Seven,Shell,Bell,Star,Wild".split(
+    ","
+  );
 
-  const spinResults = spins.map((spin, i) => reels[i][spin]);
-  const sortedItems = spinResults.sort();
-  const result = [...new Set(sortedItems)];
-  const points = itemsMap.indexOf(sortedItems[1]);
+  const sortedResults = spins.map((spin, i) => reels[i][spin]).sort();
+  const middle = sortedResults[1];
+  const points = items.indexOf(middle);
+  const differentItems = [...new Set(sortedResults)];
 
-  if (result.length === 1) return points * 10;
-  if (result.length === 2) {
-    if (sortedItems[2] === "Wild") return points * 2;
+  if (differentItems.length === 1) return points * 10;
+  if (differentItems.length === 2) {
+    if (sortedResults.filter((result) => result === "Wild").length === 1) {
+      return points * 2;
+    }
+
     return points;
   }
 
