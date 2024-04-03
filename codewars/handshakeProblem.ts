21e34b1f84504aca98d10a8309dfd21bc793387e
kasi <danielkaser@mailbox.org>
2024-04-03 11:07:32 +0200

    refactor handshakeproblem.ts

--- codewars/handshakeProblem.ts
+++ codewars/handshakeProblem.ts
@@ -2,12 +2,9 @@ export function getParticipants(handshakes: number): number {
   if (!handshakes) return 0;
 
   let sumHandshakes = 0;
-  let numPeople = 0;
 
-  for (let i = 1; ; i++) {
-    sumHandshakes += i;
-    numPeople = i;
-
-    if (sumHandshakes >= handshakes) return numPeople + 1;
+  for (let numPeople = 1; ; numPeople++) {
+    if (sumHandshakes >= handshakes) return numPeople;
+    sumHandshakes += numPeople;
   }
 }
