76b5c437c73ead8d5cac530472fe65955018fc12
kasi <danielkaser@mailbox.org>
2024-04-07 10:36:14 +0200

    refactor calculatetheareaofaregularnsidespolygoninsideacircle.ts

--- codewars/calculateTheAreaOfARegularNSidesPolygonInsideACircle.ts
+++ codewars/calculateTheAreaOfARegularNSidesPolygonInsideACircle.ts
@@ -2,8 +2,11 @@ export function areaOfPolygonInsideCircle(
   radius: number,
   numSides: number
 ): number {
-  const RADIANT_CIRCLE = 2 * Math.PI;
-  const radiantAngle = RADIANT_CIRCLE / numSides;
+  // cut polygon into triangles
+  // calc area for each triangle
+
+  const RADIANT_CIRCLE = 2 * Math.PI; // all angles
+  const radiantAngle = RADIANT_CIRCLE / numSides; // each angle
 
   const areaTriangle = 0.5 * radius * radius * Math.sin(radiantAngle);
   const areaPolygon = areaTriangle * numSides;
