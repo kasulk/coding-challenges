6a71f119319213c72455491b835a57861a43484e
kasi <danielkaser@mailbox.org>
2024-04-07 10:34:27 +0200

    solve nodejsintro.ts

---
+++ codewars/nodeJsIntro.js
@@ -0,0 +1,9 @@
+const { Buffer } = require("buffer");
+
+String.prototype.toBase64 = function () {
+  return Buffer.from(this).toString("base64");
+};
+
+String.prototype.fromBase64 = function () {
+  return Buffer.from(this, "base64").toString();
+};
