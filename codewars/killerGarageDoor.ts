b0f77c13397882fb70a91c520f3cf9a99fe056e2
kasi <danielkaser@mailbox.org>
2024-04-25 11:12:14 +0200

    refactor killergaragedoor.ts

--- codewars/killerGarageDoor.ts
+++ codewars/killerGarageDoor.ts
@@ -1,61 +1,61 @@
 export function door(events: string): string {
   type Status = "open" | "closed" | "opening" | "closing" | "paused";
 
-  let status: Status = "closed";
+  let currStatus: Status = "closed";
   let prevStatus: Status = "closed";
   let count = 0;
 
   return [...events]
     .map((event) => {
-      if (count === 0) status = "closed";
-      if (count === 5) status = "open";
+      if (count === 0) currStatus = "closed";
+      if (count === 5) currStatus = "open";
 
-      if (status === "paused") {
-        if (event === ".") return count;
-
-        if (event === "P") status = prevStatus;
-        if (status === "opening") return ++count;
-        if (status === "closing") return --count;
-      }
-
-      if (status === "closed") {
+      if (currStatus === "closed") {
         if (event === ".") return 0;
         if (event === "P") {
-          status = "opening";
+          currStatus = "opening";
           return ++count;
         }
       }
-      if (status === "open") {
+      if (currStatus === "open") {
         if (event === ".") return 5;
         if (event === "P") {
-          status = "closing";
+          currStatus = "closing";
           return --count;
         }
       }
 
-      if (status === "opening") {
+      if (currStatus === "opening") {
         if (event === ".") return ++count;
         if (event === "P") {
-          prevStatus = status;
-          status = "paused";
+          prevStatus = currStatus;
+          currStatus = "paused";
           return count;
         }
         if (event === "O") {
-          status = "closing";
+          currStatus = "closing";
           return --count;
         }
       }
-      if (status === "closing") {
+      if (currStatus === "closing") {
         if (event === ".") return --count;
         if (event === "P") {
-          prevStatus = status;
-          status = "paused";
+          prevStatus = currStatus;
+          currStatus = "paused";
           return count;
         }
         if (event === "O") {
-          status = "opening";
+          currStatus = "opening";
           return ++count;
         }
+      }
+
+      if (currStatus === "paused") {
+        if (event === ".") return count;
+
+        if (event === "P") currStatus = prevStatus;
+        if (currStatus === "opening") return ++count;
+        if (currStatus === "closing") return --count;
       }
     })
     .join("");
