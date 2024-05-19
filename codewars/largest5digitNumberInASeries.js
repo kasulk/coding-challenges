c36b7ece1246ad1076a51acbcba34c2255ef5899
kasi <danielkaser@mailbox.org>
2024-05-19 12:09:03 +0200

    solve largest5digitsnumberinaseries.js

---
+++ codewars/largest5digitNumberInASeries.js
@@ -0,0 +1,21 @@
+function solution(digits) {
+  let max = 0;
+
+  for (let i = 9; i > 0; i--) {
+    for (let j = 0; j < digits.length; j++) {
+      const currDigit = Number(digits[j]);
+
+      if (currDigit === i) {
+        const str5digits = digits.slice(j, j + 5);
+        const num5digits = Number(str5digits);
+
+        if (str5digits.length === 5) {
+          if (num5digits > max) max = num5digits;
+        }
+      }
+    }
+    if (max > 0) return max;
+  }
+
+  return max;
+}
