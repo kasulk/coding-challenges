ba8e9626ef6779a390849ca6fb8df0904e33ae8d
kasi <danielkaser@mailbox.org>
2024-05-31 10:47:41 +0200

    refactor writenumberinexpandedformpart2.js

--- codewars/writeNumberInExpandedFormPart2.js
+++ codewars/writeNumberInExpandedFormPart2.js
@@ -3,18 +3,16 @@ function expandedForm(num) {
   return [...expandInteger(integer), ...expandDecimals(decimals)].join(" + ");
 }
 
-function expandDecimals(strNum) {
-  const digits = strNum.split("");
+function expandInteger([...digits]) {
   return digits
-    .map((digit, i) => (digit === "0" ? null : `${digit}/1`.padEnd(i + 4, "0")))
+    .map((digit, i, arr) =>
+      digit === "0" ? null : digit.padEnd(arr.length - i, "0")
+    )
     .filter((expDigit) => expDigit);
 }
 
-function expandInteger(strNum) {
-  const digits = strNum.split("");
+function expandDecimals([...digits]) {
   return digits
-    .map((digit, i) =>
-      digit === "0" ? null : digit.padEnd(digits.length - i, "0")
-    )
+    .map((digit, i) => (digit === "0" ? null : `${digit}/1`.padEnd(i + 4, "0")))
     .filter((expDigit) => expDigit);
 }
