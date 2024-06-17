0f2a2739cb42a89ca697891ca88a827a6810b01e
kasi <danielkaser@mailbox.org>
2024-06-17 11:41:44 +0200

    refactor highestranknumberinanarray.js

--- codewars/highestRankNumberInAnArray.js
+++ codewars/highestRankNumberInAnArray.js
@@ -1,13 +1,13 @@
-function highestRank(arr) {
+function highestRank(nums) {
   let numsCount = {};
 
-  arr.forEach((num) => {
+  nums.forEach((num) => {
     if (!numsCount[num]) numsCount[num] = 0;
     numsCount[num]++;
   });
 
-  const numCounts = Object.entries(numsCount).sort((a, b) => b[1] - a[1]);
-  const maxCount = numCounts[0][1];
+  const numCounts = Object.entries(numsCount);
+  const maxCount = numCounts.sort((a, b) => b[1] - a[1])[0][1];
 
   const maxNums = numCounts
     .filter(([_, count]) => count === maxCount)
