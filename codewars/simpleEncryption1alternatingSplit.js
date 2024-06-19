8bd791d34c1a2ca4e42c3f074a2dead4cd77ee1f
kasi <danielkaser@mailbox.org>
2024-06-19 11:35:29 +0200

    refactor simpleencryption1alternatingsplit.js

--- codewars/simpleEncryption1alternatingSplit.js
+++ codewars/simpleEncryption1alternatingSplit.js
@@ -1,41 +1,40 @@
 function encrypt(str, n) {
   if (!str || n <= 0) return str;
-
-  let odds;
-  let evens;
   let result = str;
 
   for (let j = 0; j < n; j++) {
-    odds = "";
-    evens = "";
+    let odds = "";
+    let evens = "";
+
     for (let j = 0; j < result.length; j++) {
       const curr = result[j];
       if (j % 2 === 0) evens += curr;
       else odds += curr;
     }
+
     result = odds + evens;
   }
+
   return result;
 }
 
 function decrypt(str, n) {
   if (!str || n <= 0) return str;
-  console.log("str,n:", str, n);
-
-  //
-  let result;
-  let stepResult = str;
+  let result = str;
 
   for (let i = 0; i < n; i++) {
-    result = "";
-    for (let j = 0; j < str.length / 2; j++) {
-      const firstIndex = Math.floor(str.length / 2) + j;
-      result += stepResult[firstIndex];
+    let stepResult = "";
 
-      if (result.length === str.length) break;
-      result += stepResult[j];
+    for (let j = 0; j < str.length / 2; j++) {
+      const evensIndex = Math.floor(str.length / 2) + j;
+      stepResult += result[evensIndex];
+
+      if (stepResult.length === str.length) break;
+      stepResult += result[j];
     }
-    stepResult = result;
+
+    result = stepResult;
   }
-  return stepResult;
+
+  return result;
 }
