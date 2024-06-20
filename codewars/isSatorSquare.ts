96c4e220d26c4666a5ff8da9d30b9a4119f37028
kasi <danielkaser@mailbox.org>
2024-06-20 11:03:32 +0200

    solve issatorsquare.ts

---
+++ codewars/isSatorSquare.ts
@@ -0,0 +1,26 @@
+export function isSatorSquare(tablet: string[][]): boolean {
+  for (let i = 0; i < tablet.length; i++) {
+    const leftToRight = tablet[i].join("");
+    const topToBottom = tablet.map((row) => row[i]).join("");
+    const bottomToTop = tablet
+      .slice()
+      .map((row) => row[row.length - 1 - i])
+      .reverse()
+      .join("");
+    const rightToLeft = tablet[tablet.length - 1 - i]
+      .slice()
+      .reverse()
+      .join("");
+
+    if (
+      !(
+        leftToRight === topToBottom &&
+        topToBottom === bottomToTop &&
+        bottomToTop === rightToLeft
+      )
+    )
+      return false;
+  }
+
+  return true;
+}
