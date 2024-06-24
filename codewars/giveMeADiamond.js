8bf7e30346cf51818bb5db32dd141ba7c09bb323
kasi <danielkaser@mailbox.org>
2024-06-24 11:23:09 +0200

    refactor givemeadiamond.js

--- codewars/giveMeADiamond.js
+++ codewars/giveMeADiamond.js
@@ -1,23 +1,22 @@
 function diamond(n) {
   if (n % 2 === 0 || n <= 0) return null;
-  if (n === 1) return "*\n";
+
   const roughDiamond = [];
 
   for (let i = 1; i <= n - 2; i += 2) {
     const spaces = " ".repeat((n - i) / 2);
     const asterisks = "*".repeat(i);
-    const layer = spaces + asterisks;
-    roughDiamond.push(layer);
+    roughDiamond.push(spaces + asterisks);
   }
 
-  roughDiamond.push("*".repeat(n));
+  const middleLayer = "*".repeat(n);
+  roughDiamond.push(middleLayer);
 
   for (let i = n - 2; i >= 1; i -= 2) {
     const spaces = " ".repeat((n - i) / 2);
     const asterisks = "*".repeat(i);
-    const layer = spaces + asterisks;
-    roughDiamond.push(layer);
+    roughDiamond.push(spaces + asterisks);
   }
 
-  return roughDiamond.join("\n").replace(/\s*$/, "\n");
+  return roughDiamond.join("\n") + "\n";
 }
