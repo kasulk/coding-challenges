5f59738d096e5b6d56eda61193f5652392de2917
kasi <danielkaser@mailbox.org>
2024-06-27 10:50:40 +0200

    solve thehashtaggenerator.js

---
+++ codewars/theHashtagGenerator.js
@@ -0,0 +1,14 @@
+function generateHashtag(str) {
+  const words = str.trim().replace(/\s+/g, " ");
+
+  if (!words || words === " ") return false;
+
+  const hashtag =
+    "#" +
+    words
+      .split(" ")
+      .map((word) => word[0].toUpperCase() + word.slice(1).toLowerCase())
+      .join("");
+
+  return hashtag.length > 140 ? false : hashtag;
+}
