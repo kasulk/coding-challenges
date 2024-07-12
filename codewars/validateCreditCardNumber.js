951155496337fa2a2a3c06598151ebd4a78430e1
kasi <danielkaser@mailbox.org>
2024-07-12 10:49:21 +0200

    refactor validatecreditcardnumber.js

--- codewars/validateCreditCardNumber.js
+++ codewars/validateCreditCardNumber.js
@@ -1,10 +1,11 @@
-function validate(n) {
-  const digits = n.toString().split("");
-  const revDigits = digits
+function validate(num) {
+  const digitsSum = num
+    .toString()
+    .split("")
     .reverse()
     .map((digit, i) => (i % 2 === 0 ? Number(digit) : digit * 2))
     .map((digit) => (digit >= 10 ? digit - 9 : digit))
     .reduce((sum, digit) => sum + digit, 0);
 
-  return revDigits % 10 === 0;
+  return digitsSum % 10 === 0;
 }
