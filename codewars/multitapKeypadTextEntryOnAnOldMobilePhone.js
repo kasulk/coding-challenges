28238169e8b18038c6fc54e5344c56fa06ac74f8
kasi <danielkaser@mailbox.org>
2024-08-12 11:13:22 +0200

    solve multitapkeypadtextentryonanoldmobilephone.js

---
+++ codewars/multitapKeypadTextEntryOnAnOldMobilePhone.js
@@ -0,0 +1,32 @@
+function presses(phrase) {
+  const keypad = [
+    ["1"],
+    ["ABC2"],
+    ["DEF3"],
+    ["GHI4"],
+    ["JKL5"],
+    ["MNO6"],
+    ["PQRS7"],
+    ["TUV8"],
+    ["WXYZ9"],
+    ["*"],
+    [" 0"],
+    ["#"],
+  ];
+
+  let numPresses = 0;
+
+  return phrase
+    .toUpperCase()
+    .split("")
+    .reduce((sum, char) => {
+      for (const button of keypad) {
+        const buttonChars = button[0].split("");
+        const charIndex = buttonChars.indexOf(char);
+        numPresses = charIndex + 1;
+        if (numPresses) break;
+      }
+
+      return numPresses ? sum + numPresses : sum;
+    }, 0);
+}
