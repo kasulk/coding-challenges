1dd8e37f0be6a2eaf9e53901d192dbe0f06c72b5
kasi <danielkaser@mailbox.org>
2024-08-18 08:51:26 +0200

    refactor sortbylastchar.js

--- codewars/sortByLastChar.js
+++ codewars/sortByLastChar.js
@@ -1,10 +1,3 @@
 function last(str) {
-  return str
-    .slice()
-    .split(" ")
-    .map((word) => word.split("").reverse().join(""))
-    .sort((a, b) => {
-      if (b[0] > a[0]) return -1;
-    })
-    .map((word) => word.split("").reverse().join(""));
+  return str.split(" ").sort((a, b) => (a.slice(-1) < b.slice(-1) ? -1 : 0));
 }
