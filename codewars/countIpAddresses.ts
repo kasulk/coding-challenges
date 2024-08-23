3d67b0722a5ee6ec93238fcfbabab6084bd7715f
kasi <danielkaser@mailbox.org>
2024-08-23 11:08:58 +0200

    refactor countipaddresses.ts

--- codewars/countIpAddresses.ts
+++ codewars/countIpAddresses.ts
@@ -2,13 +2,8 @@ export function ipsBetween(start: string, end: string): number {
   const startNums = start.split(".").map(Number);
   const endNums = end.split(".").map(Number);
 
-  const differences = startNums.map((startNum, i) => {
-    return endNums[i] - startNum;
-  });
-
-  const result = differences.reverse().reduce((acc, curr, i) => {
-    return acc + curr * 256 ** i;
-  }, 0);
-
-  return result;
+  return startNums
+    .map((startNum, i) => endNums[i] - startNum)
+    .reverse()
+    .reduce((accSum, currNum, i) => accSum + currNum * 256 ** i, 0);
 }
