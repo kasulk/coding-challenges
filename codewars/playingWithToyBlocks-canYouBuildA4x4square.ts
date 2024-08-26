8cace749794de88f17cbf902a6552cdac3d2b664
kasi <danielkaser@mailbox.org>
2024-08-26 16:43:21 +0200

    refactor playingwithtoyblocks-canyoubuilda4x4square.ts

--- codewars/playingWithToyBlocks-canYouBuildA4x4square.ts
+++ codewars/playingWithToyBlocks-canYouBuildA4x4square.ts
@@ -1,9 +1,5 @@
 export function buildSquare(blocks: number[]): boolean {
-  let numRows = 0;
-  const num4s = blocks.filter((num) => num === 4).length;
-  const num3s = blocks.filter((num) => num === 3).length;
-  const num2s = blocks.filter((num) => num === 2).length;
-  const num1s = blocks.filter((num) => num === 1).length;
+  const [num1s, num2s, num3s, num4s] = getBlockCounts(blocks);
 
   const numRowsOf3sAnd1s = Math.min(num3s, num1s);
   let rest1s = num1s - numRowsOf3sAnd1s;
@@ -11,14 +7,19 @@ export function buildSquare(blocks: number[]): boolean {
   const numRowsOf2s = Math.trunc(num2s / 2);
   const rest2s = num2s - numRowsOf2s * 2;
 
-  let numRowsOf2sAnd1s = Math.min(rest2s, rest1s * 2);
-  numRowsOf2sAnd1s = numRowsOf2sAnd1s >= 1 ? Math.trunc(numRowsOf2sAnd1s) : 0;
-
+  const numRowsOf2sAnd1s = Math.min(rest2s, Math.trunc(rest1s / 2));
   rest1s = rest1s - numRowsOf2sAnd1s * 2;
 
   const numRowsOf1s = Math.trunc(rest1s / 4);
-  numRows =
+
+  const sumRows =
     num4s + numRowsOf3sAnd1s + numRowsOf2s + numRowsOf2sAnd1s + numRowsOf1s;
 
-  return numRows >= 4;
+  return sumRows >= 4;
+}
+
+function getBlockCounts(blocks: number[]): number[] {
+  return [1, 2, 3, 4].map(
+    (num) => blocks.filter((blocksNum) => num === blocksNum).length
+  );
 }
