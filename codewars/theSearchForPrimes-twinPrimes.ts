6271de1246d1c55577311581933b9c22acc6cbe8
kasi <danielkaser@mailbox.org>
2024-09-04 10:12:32 +0200

    solve thesearchforprimes-twinprimes.ts

---
+++ codewars/theSearchForPrimes-twinPrimes.ts
@@ -0,0 +1,19 @@
+export function twinPrime(n: number): number {
+  let numTwinPrimes = 0;
+
+  for (let i = 2; i < n; i++) {
+    if (isPrime(i) && isPrime(i + 2)) numTwinPrimes++;
+  }
+
+  return numTwinPrimes;
+}
+
+function isPrime(n: number): boolean {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+twinPrime(11);
