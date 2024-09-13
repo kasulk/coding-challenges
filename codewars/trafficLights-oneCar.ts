7116e7dc07e6accc6b75753d71ec9571cce73b4b
kasi <danielkaser@mailbox.org>
2024-09-13 13:20:49 +0200

    refactor trafficlights-onecar.ts

--- codewars/trafficLights-oneCar.ts
+++ codewars/trafficLights-oneCar.ts
@@ -3,60 +3,60 @@ export function trafficLights(road: string, n: number): string[] {
 
   let result: string[] = [road];
   let currRoad = road.split("");
-  let newRoad: string[] = [];
 
   for (let i = 1; i <= n; i++) {
-    for (let j = 0; j < currRoad.length; j++) {
-      const prevCell = currRoad[j - 1];
-      const currCell = currRoad[j];
-      let updatedTrafficLight: string;
+    const roadWithUpdatedTrafficLights = updateTrafficLights(road, i)
+      .replace("C", ".")
+      .split("");
 
-      const isCurrTrafficLight = "GOR".includes(road[j]);
-      const isNextTrafficLight = "GOR".includes(road[j + 1]);
+    const nextRoad = [...roadWithUpdatedTrafficLights];
+    const carPosition = currRoad.indexOf("C");
+    const nextCell = roadWithUpdatedTrafficLights[carPosition + 1];
 
-      if (isCurrTrafficLight) {
-        updatedTrafficLight = updateTrafficLight(road[j], i) || "";
-        if (prevCell === "C" && updatedTrafficLight === "G") newRoad.push("C");
-        else newRoad.push(updatedTrafficLight);
-        continue;
-      }
+    if (carPosition < 0) {
+      result.push(nextRoad.join(""));
+      continue;
+    }
 
-      if (currCell === ".") {
-        if (prevCell === "C") newRoad.push("C");
-        else newRoad.push(".");
-      }
+    if (nextCell === "R" || nextCell === "O") {
+      nextRoad[carPosition] = "C";
+      result.push(nextRoad.join(""));
+      continue;
+    }
 
-      if (currCell === "C") {
-        if (isNextTrafficLight) {
-          updatedTrafficLight = updateTrafficLight(road[j + 1], i) || "";
-          if (updatedTrafficLight === "G") newRoad.push(".");
-          else newRoad.push("C");
-          continue;
-        }
+    if (nextCell === "." || nextCell === "G") {
+      const isIntersection = "GOR".includes(road[carPosition]);
+      nextRoad[carPosition] = ".";
+      nextRoad[carPosition + 1] = "C";
 
-        if (isCurrTrafficLight) newRoad.push("G");
-        else newRoad.push(".");
+      if (isIntersection) {
+        nextRoad[carPosition] = roadWithUpdatedTrafficLights[carPosition];
       }
     }
 
-    currRoad = newRoad;
-    newRoad = [];
-    result.push(currRoad.join(""));
+    result.push(nextRoad.join(""));
+    currRoad = [...nextRoad];
   }
 
   return result;
 }
 
-function updateTrafficLight(initialStatus: string, n: number): string | void {
-  if (initialStatus === "R") {
+function updateTrafficLight(initialColor: string, n: number): string {
+  if (initialColor === "R") {
     if (n % 11 < 5) return "R";
     if (n % 11 < 10) return "G";
     return "O";
   }
 
-  if (initialStatus === "G") {
+  if (initialColor === "G") {
     if (n % 11 < 5) return "G";
     if (n % 11 === 5) return "O";
     return "R";
   }
+
+  return "";
+}
+
+function updateTrafficLights(initialRoad: string, n: number): string {
+  return initialRoad.replace(/[GOR]/g, (v) => updateTrafficLight(v, n));
 }
