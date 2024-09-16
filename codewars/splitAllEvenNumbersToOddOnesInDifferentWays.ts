59e7e7662988e7519f28df9889860abac60ecd2f
kasi <danielkaser@mailbox.org>
2024-09-16 10:35:09 +0200

    refactor splitallevennumberstooddonesindifferentways.ts

--- codewars/splitAllEvenNumbersToOddOnesInDifferentWays.ts
+++ codewars/splitAllEvenNumbersToOddOnesInDifferentWays.ts
@@ -1,23 +1,23 @@
 export function splitAllEvenNumbers(nums: number[], way: number): number[] {
   return nums.flatMap((num) => {
     if (!isEven(num)) return num;
-    if (way === 0) return splitEvenNumToClosestOdds(num);
-    if (way === 1) return splitEvenNumToFarestOdds(num);
-    if (way === 2) return splitEvenNumToEqualAndMaxPossible(num);
+    if (way === 0) return splitEvenNumToOddsClosestToEachOther(num);
+    if (way === 1) return splitEvenNumToOddsFarestToEachOther(num);
+    if (way === 2) return splitEvenNumToEqualOddsAndMaxPossible(num);
     return splitEvenNumToAllOnes(num);
   });
 }
 
-function splitEvenNumToClosestOdds(num: number): number[] {
+function splitEvenNumToOddsClosestToEachOther(num: number): number[] {
   const half = num / 2;
   return isEven(half) ? [half - 1, half + 1] : [half, half];
 }
 
-function splitEvenNumToFarestOdds(num: number): number[] {
+function splitEvenNumToOddsFarestToEachOther(num: number): number[] {
   return [1, num - 1];
 }
 
-function splitEvenNumToEqualAndMaxPossible(num: number): number[] {
+function splitEvenNumToEqualOddsAndMaxPossible(num: number): number[] {
   let half = num / 2;
   while (isEven(half)) half /= 2;
   return Array(num / half).fill(half);
