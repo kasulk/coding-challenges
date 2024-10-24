17568a420732a55213506d900190049942dd23f4
kasi <danielkaser@mailbox.org>
2024-10-24 10:25:29 +0200

    refactor nthpowerrulesthemall.js

--- codewars/nthPowerRulesThemAll.js
+++ codewars/nthPowerRulesThemAll.js
@@ -1,6 +1,3 @@
 function modifiedSum(nums, n) {
-  const poweredSum = nums.reduce((acc, curr) => acc + curr ** n, 0);
-  const sum = nums.reduce((acc, curr) => acc + curr, 0);
-
-  return poweredSum - sum;
+  return nums.reduce((sum, num) => sum + num ** n - num, 0);
 }
