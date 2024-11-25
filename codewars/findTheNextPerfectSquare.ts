b71114e13e5b2d63158ae3471d1d8a6130130f4c
kasi <danielkaser@mailbox.org>
2024-11-25 10:45:58 +0100

    refactor findthenextperfectsquare.ts

--- codewars/findTheNextPerfectSquare.ts
+++ codewars/findTheNextPerfectSquare.ts
@@ -1,4 +1,4 @@
 export function findNextSquare(square: number): number {
   const root = Math.sqrt(square);
-  return root % 1 === 0 ? Math.pow(root + 1, 2) : -1;
+  return Number.isInteger(root) ? Math.pow(root + 1, 2) : -1;
 }
