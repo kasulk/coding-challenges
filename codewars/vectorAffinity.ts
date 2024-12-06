944f39a97aafbb060d58e6e6e2597294bf87b7b7
kasi <danielkaser@mailbox.org>
2024-12-06 11:11:39 +0100

    refactor vectoraffinity.ts

--- codewars/vectorAffinity.ts
+++ codewars/vectorAffinity.ts
@@ -1,15 +1,12 @@
 export function vectorAffinity(
-  xs: Array<number | null>,
-  ys: Array<number | null>
+  vec1: (number | null)[],
+  vec2: (number | null)[]
 ): number {
-  const maxLen = xs.length > ys.length ? xs.length : ys.length;
+  const maxLen = vec1.length > vec2.length ? vec1.length : vec2.length;
 
-  if (!maxLen) return 1;
-
-  const sameValueCount = xs.reduce<number>((acc, currX, i) => {
-    const currY = ys[i];
-    return currX === currY ? acc + 1 : acc;
+  const sameValueCount = vec1.reduce((acc: number, _, i) => {
+    return vec1[i] === vec2[i] ? acc + 1 : acc;
   }, 0);
 
-  return sameValueCount / maxLen;
+  return maxLen ? sameValueCount / maxLen : 1;
 }
