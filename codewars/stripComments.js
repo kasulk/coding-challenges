959f098ed25d126675e32bc00c7221f0993e60e0
kasi <danielkaser@mailbox.org>
2024-12-09 11:25:58 +0100

    solve stripcomments.js

---
+++ codewars/stripComments.js
@@ -0,0 +1,17 @@
+function solution(text, markers) {
+  return text
+    .split("\n")
+    .map((line) => {
+      for (let i = 0; i < line.length; i++) {
+        const char = line[i];
+        if (markers.includes(char)) return line.slice(0, i).trimEnd();
+      }
+      return line.trimEnd();
+    })
+    .join("\n")
+    .trimEnd();
+}
+
+String.prototype.trimEnd = function () {
+  return this.replace(/[ ]+$/, "");
+};
