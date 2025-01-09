7f5c9e4d210d28b3a290660e0e57d593db28208a
kasi <danielkaser@mailbox.org>
2025-01-09 12:35:08 +0100

    refactor simpleencryption2indexdifference.ts; refactor decrypt()

--- codewars/simpleEncryption2indexDifference.ts
+++ codewars/simpleEncryption2indexDifference.ts
@@ -31,36 +31,31 @@ export function encrypt(text: string): string {
 }
 
 export function decrypt(encryptedText: string): string {
-  console.log("encryptedText:", encryptedText);
-
   if (encryptedText === null || encryptedText === "") return encryptedText;
 
-  const encryptedChars = encryptedText.split("");
-  let result = "";
+  const decryptedText = encryptedText
+    .split("")
+    .reduce((result, currChar, i) => {
+      let decryptedChar = "";
+      if (!charRegion.includes(currChar)) throw new Error("möp!");
 
-  for (let i = 0; i < encryptedChars.length; i++) {
-    const char = encryptedChars[i];
-    if (!charRegion.includes(char)) throw new Error("möp!");
+      if (!i) decryptedChar = getMirror(currChar);
+      else {
+        const prevChar = result[i - 1];
+        const prevCharIndex = charRegion.indexOf(prevChar);
+        const currCharIndex = charRegion.indexOf(currChar);
 
-    if (!i) {
-      result += getMirror(char);
-      continue;
-    }
+        const regionIndexDiff = currCharIndex - charRegion.length;
+        const decryptedCharIndex =
+          (prevCharIndex - regionIndexDiff) % charRegion.length;
+        decryptedChar = charRegion[decryptedCharIndex];
+      }
 
-    const prev = result[i - 1];
-    const prevIndex = charRegion.indexOf(prev);
+      return [...result, decryptedChar];
+    }, [] as string[])
+    .join("");
 
-    const charIndex = charRegion.indexOf(char);
-    const regionIndexDiff = charIndex - charRegion.length;
-
-    const decryptedCharIndex =
-      (prevIndex - regionIndexDiff) % charRegion.length;
-    const decryptedChar = charRegion[decryptedCharIndex];
-
-    result += decryptedChar;
-  }
-
-  return switchCaseEverySecondChar(result);
+  return switchCaseEverySecondChar(decryptedText);
 }
 
 function switchCase(char: string): string {
