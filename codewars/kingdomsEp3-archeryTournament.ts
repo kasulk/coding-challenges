6d3fb4e24524cb1f369316a72719527c6fc16ebb
kasi <danielkaser@mailbox.org>
2025-01-25 17:40:43 +0100

    refactor kingdomep3-archerytournament.ts

--- codewars/kingdomsEp3-archeryTournament.ts
+++ codewars/kingdomsEp3-archeryTournament.ts
@@ -4,41 +4,13 @@ type Arrow = { char: string; score: number; num: number };
 type ArrowDict = { [key: Letter]: Arrow };
 
 export function countAndSort(target: string[]): string[] {
-  const scoreDict = createTargetScoreDict(target.length);
-  const arrowDict: ArrowDict = {};
-
-  // loop through target
-  target.forEach((row, i) => {
-    row.split("").forEach((curr, j) => {
-      let multiplier = 1;
-      // lookup score for each arrow in the dict
-      if (isArrow(curr)) {
-        if (isUpperCase(curr)) {
-          multiplier = 2;
-          curr = curr.toLowerCase();
-        }
-
-        const score = scoreDict[i][j];
-
-        if (!arrowDict[curr]) {
-          arrowDict[curr] = { char: curr, score: 0, num: 0 };
-        }
-        arrowDict[curr].score += score * multiplier;
-        arrowDict[curr].num += 1 * multiplier;
-      }
-    });
-  });
-
-  // console.log("arrowDict:", arrowDict);
-
-  const sortedArrows = sortArrowLettersByScoreOrQuantityOrLetters(arrowDict);
-
-  console.log("sortedArrows:", sortedArrows);
-
+  const scoreDict = createScoreDict(target.length);
+  const arrowDict = createArrowDict(target, scoreDict);
+  const sortedArrows = sortArrowsByScoreOrQuantityOrLetters(arrowDict);
   return sortedArrows.map(({ char }) => char);
 }
 
-function createTargetScoreDict(width: number): number[][] {
+function createScoreDict(width: number): number[][] {
   const halfWidth = width / 2;
   const halfWidthRoundUp = Math.ceil(halfWidth);
   const firstHalfOfCenterRow = Array(halfWidthRoundUp)
@@ -66,9 +38,34 @@ function createTargetScoreDict(width: number): number[][] {
   return [...bottomHalfRows.slice(1).reverse(), ...bottomHalfRows];
 }
 
-function sortArrowLettersByScoreOrQuantityOrLetters(
-  arrowDict: ArrowDict
-): Arrow[] {
+function createArrowDict(target: string[], scoreDict: number[][]): ArrowDict {
+  const arrowDict: ArrowDict = {};
+
+  target.forEach((row, i) => {
+    row.split("").forEach((curr, j) => {
+      let multiplier = 1;
+
+      if (isArrow(curr)) {
+        if (isUpperCase(curr)) {
+          multiplier = 2;
+          curr = curr.toLowerCase();
+        }
+
+        const score = scoreDict[i][j];
+
+        if (!arrowDict[curr]) {
+          arrowDict[curr] = { char: curr, score: 0, num: 0 };
+        }
+        arrowDict[curr].score += score * multiplier;
+        arrowDict[curr].num += 1 * multiplier;
+      }
+    });
+  });
+
+  return arrowDict;
+}
+
+function sortArrowsByScoreOrQuantityOrLetters(arrowDict: ArrowDict): Arrow[] {
   return Object.values(arrowDict).sort((a, b) => {
     if (a.score === b.score) {
       if (a.num === b.num) return a.char.localeCompare(b.char);
@@ -85,39 +82,3 @@ function isUpperCase(char: string): boolean {
 function isArrow(char: string): boolean {
   return char !== "*";
 }
-
-countAndSort(["z**", "a**", "**z"]); //, ["a", "z"]);
-
-// countAndSort(["z**", "*a*", "***"]); //, ["z", "a"]);
-
-// countAndSort(["z*B", "*a*", "**z"]); //, ["b", "z", "a"]);
-
-// countAndSort(["a****", "*****", "**z**", "a*k**", "****z"]); //, ["a", "k", "z"]);
-
-// countAndSort([ "a*****S", "*******", "**z****", "a*kOA**", "***L**z", "*****gG", "******z", ]);
-//, ['s', 'k', 'g', 'z', 'l', 'a', 'o']);
-
-// countAndSort(["*a*", "***", "*b*"]); //, ["a", "b"]);
-
-// countAndSort(["*A*", "*b*", "***"]); //, ["a", "b"]);
-
-// countAndSort(["A"]); //, ["a"]);
-
-// countAndSort(["*"]); //, []);
-
-// countAndSort([ "**f*c*f**", "*Yf*yAF**", "Z*D***e**", "*dy**d*d*", "**Ze***Dd", "**Z*e*Y*A", "Da*yfC***", "F***Y*z**", "zez***f**", ]);
-//, [ "c", "a", "e", "f", "z", "d", "y"]);
-
-// 11 1 11 // 0
-// 12 2 21 // 1
-// 12 3 21 // 2
-// 12 2 21 // 3
-// 11 1 11 // 4
-
-// 111 1 111 // 0
-// 122 2 221 // 1
-// 123 3 321 // 2
-// 123 4 321 // 3
-// 123 3 321 // 4
-// 122 2 221 // 5
-// 111 1 111 // 6
