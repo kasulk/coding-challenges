8a0a5ea8304942a28288518fb544c46a813b008e
kasi <danielkaser@mailbox.org>
2025-02-01 15:16:57 +0100

    refactor consecutiveitems.js

--- codewars/consecutiveItems.js
+++ codewars/consecutiveItems.js
@@ -2,9 +2,8 @@ function consecutive(nums, a, b) {
   for (let i = 0; i < nums.length; i++) {
     const curr = nums[i];
     const next = nums[i + 1];
-    if ((curr === a && next === b) || (curr === b && next === a)) {
-      return true;
-    }
+    if (curr === a && next === b) return true;
+    if (curr === b && next === a) return true;
   }
   return false;
 }
