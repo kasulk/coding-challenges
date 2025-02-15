a7f31de726918f69d09153e974721a6bb44bc300
kasi <danielkaser@mailbox.org>
2025-02-15 11:51:19 +0100

    refactor wellofideas-harderversion.php

--- codewars/wellOfIdeas-harderVersion.php
+++ codewars/wellOfIdeas-harderVersion.php
@@ -1,18 +1,17 @@
 <?php
-function well($arr): string {
-    $num_good_ideas = 0;
+function well(array $arrays): string {
+  $num_good_ideas = 0;
 
-    for ($i = 0; $i < count($arr); $i++) {
-        $curr_sub_array = $arr[$i];
+  for ($i = 0; $i < count($arrays); $i++) {
+    $sub_array = $arrays[$i];
 
-        for ($j = 0; $j < count($curr_sub_array); $j++) {
-            $curr_element = $curr_sub_array[$j];
-
-            if (strtolower($curr_element) === 'good') $num_good_ideas++;
-        }
+    for ($j = 0; $j < count($sub_array); $j++) {
+      $element = $sub_array[$j];
+      if (strtolower($element) === 'good') $num_good_ideas++;
     }
+  }
 
-    if ($num_good_ideas > 2) return 'I smell a series!';
-    if ($num_good_ideas > 0) return 'Publish!';
-    return 'Fail!';
+  if ($num_good_ideas > 2) return 'I smell a series!';
+  if ($num_good_ideas > 0) return 'Publish!';
+  return 'Fail!';
 }
