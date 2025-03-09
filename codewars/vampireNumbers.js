4ee77a6563408c8b2957c11ad31aaaa65fcbf4b2
kasi <danielkaser@mailbox.org>
2025-03-09 16:20:42 +0100

    refactor vampirenumbers.js

--- codewars/vampireNumbers.js
+++ codewars/vampireNumbers.js
@@ -1,8 +1,5 @@
 function vampireTest(a, b) {
-  const product = a * b;
-  const sortedProductDigits = product.toString().split("").sort();
-  const aDigits = a.toString().split("");
-  const bDigits = b.toString().split("");
-  const sortedMultiplicantDigits = [...aDigits, ...bDigits].sort();
-  return sortedProductDigits.join("") === sortedMultiplicantDigits.join("");
+  const sortedProduct = (a * b).toString().split("").sort().join("");
+  const sortedMultiplicants = (a.toString() + b.toString()).split("").sort().join("");
+  return sortedProduct === sortedMultiplicants;
 }
