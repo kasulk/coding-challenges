c5c01cfbc6f7fb8a5eb2f6e842836fdf17879b7f
kasi <danielkaser@mailbox.org>
2025-03-13 10:27:39 +0100

    refactor findallnonconsecutivenumbers.js

--- codewars/findAllNonConsecutiveNumbers.js
+++ codewars/findAllNonConsecutiveNumbers.js
@@ -2,9 +2,8 @@ function allNonConsecutive(nums) {
   return nums.reduce((acc, curr, i) => {
     const prev = nums[i - 1];
 
-    if (!i || curr === prev + 1) {
-      return acc;
-    }
+    if (!i || curr === prev + 1) return acc;
+
     return [...acc, { i: i, n: curr }];
   }, []);
 }
