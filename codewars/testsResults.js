0ccd986c84cae6b24e5a2e91d77732ab2cf268e6
kasi <danielkaser@mailbox.org>
2025-03-22 15:42:13 +0100

    refactor testsresults.js

--- codewars/testsResults.js
+++ codewars/testsResults.js
@@ -1,13 +1,13 @@
 function testResult(nums) {
   const sum = nums.reduce((sum, num) => sum + num, 0);
-  const avgStr = (sum / nums.length).toFixed(3);
-  const output = [Number(avgStr)];
+  const strAvg = (sum / nums.length).toFixed(3);
+  const output = [Number(strAvg)];
 
   const dict = nums.reduce(
     (acc, curr) => {
-      if (curr === 9 || curr === 10) return { ...acc, h: acc.h + 1 };
-      if (curr === 7 || curr === 8) return { ...acc, a: acc.a + 1 };
-      else return { ...acc, l: acc.l + 1 };
+      if (curr === 9 || curr === 10) return { ...acc, h: ++acc.h };
+      if (curr === 7 || curr === 8) return { ...acc, a: ++acc.a };
+      else return { ...acc, l: ++acc.l };
     },
     { h: 0, a: 0, l: 0 }
   );
