8b6783ccedfee38d332669e969768ad67935f748
kasi <danielkaser@mailbox.org>
2025-04-12 21:32:36 +0200

    refactor minmaxmin-boundednums.js

--- codewars/minMaxMin-boundedNums.js
+++ codewars/minMaxMin-boundedNums.js
@@ -1,18 +1,11 @@
 function minMinMax(nums) {
   const sortedNums = nums.sort((a, b) => a - b);
-  const min = sortedNums[0];
-  const max = sortedNums.slice(-1)[0];
-  let minAbsent;
-  console.log("sortedNums:", sortedNums);
+  const min = Math.min(...nums);
+  const max = Math.max(...nums);
 
   for (let i = 0; i < nums.length; i++) {
     const curr = nums[i];
     const next = nums[i + 1];
-    if (next !== curr && next !== curr + 1) {
-      minAbsent = curr + 1;
-      break;
-    }
+    if (next !== curr && next !== curr + 1) return [min, curr + 1, max];
   }
-
-  return [min, minAbsent, max];
 }
