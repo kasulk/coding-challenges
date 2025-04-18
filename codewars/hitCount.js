ed15043e87fd0205bdebac9437c28ceefb71bac6
kasi <danielkaser@mailbox.org>
2025-04-18 16:12:10 +0200

    refactor hitcount.js

--- codewars/hitCount.js
+++ codewars/hitCount.js
@@ -2,10 +2,9 @@ function counterEffect(hitCount) {
   return hitCount
     .split("")
     .map(Number)
-    .reduce((acc, currDigit) => {
-      const currArr = Array(currDigit + 1)
+    .map((digit) =>
+      Array(digit + 1)
         .fill(0)
-        .map((_, i) => i);
-      return [...acc, currArr];
-    }, []);
+        .map((_, i) => i)
+    );
 }
